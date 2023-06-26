4207e7a5f492b8458c74a436bc1b1c933a675253
Gianni Narracci <gnarracci@gmail.com>
2023-06-26 03:08:44 -0400

    First Route Test Config

--- api-backend/src/app.ts
+++ api-backend/src/app.ts
@@ -1,6 +1,15 @@
 import express, { Application } from 'express';
+import morgan from 'morgan';
+
+import authRoutes from './routes/auth';
 
 const app: Application = express();
+
+// Middlewares
+app.use(morgan('dev'));
+
+// Routes
+app.use(authRoutes);
 
 // Settings
 app.set('port', 3000);
