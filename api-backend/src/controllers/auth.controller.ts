f31082e7314e3a36532db424b1725af48d9a87d8
Gianni Narracci <gnarracci@gmail.com>
2023-07-12 02:32:59 -0400

    Token Validation config

--- api-backend/src/controllers/auth.controller.ts
+++ api-backend/src/controllers/auth.controller.ts
@@ -10,16 +10,30 @@ export const signup = async (req: Request, res: Response) => {
         email: req.body.email,
         password: req.body.password
     })
+    user.password = await user.encryptPassword(user.password);
     const saveduser = await user.save();
      // Token
-     const token: string = jwt.sign({_id: saveduser._id}, process.env.TOKEN_SECRET || 'token_secret');
+     const token: string = jwt.sign({_id: saveduser._id}, process.env.TOKEN_SECRET || 'token_secret', {
+        expiresIn: 60 * 60
+     });
     res.header('auth-token', token).json(saveduser);
 };
 
-export const signin = (req: Request, res: Response) => {
-    res.send('signin');
+export const signin = async (req: Request, res: Response) => {
+   const user = await User.findOne({email: req.body.email});
+   if(!user) return res.status(400).json("User is wrong or not exits");
+
+   const correctPassword: boolean = await user.validatePassword(req.body.password);
+   if(!correctPassword) return res.status(400).json("Invalid Password");
+   
+   const token: string = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET || "token_secret", {
+    expiresIn: 60 * 60
+   });
+   res.header('auth-token', token).json(user);
 };
 
-export const profile = (req: Request, res: Response) => {
-    res.send('profile');
+export const profile = async (req: Request, res: Response) => {
+    const user = await User.findById(req.userId, { password: 0});
+    if(!user) return res.status(404).json("No User found");
+    res.json(user);
 }
