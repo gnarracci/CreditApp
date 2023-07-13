64609889f3cfcea5278adcc6ce98ec3dd263a0cf
Gianni Narracci <gnarracci@gmail.com>
2023-07-13 10:03:10 -0400

    Initial Comp Config

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SigninComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignupComponent,
+    SigninComponent,
+    DashboardComponent,
+    NotfoundComponent,
+    CustomerComponent,
+    ConfigurationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
